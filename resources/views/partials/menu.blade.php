628577fb6b35bcb23ef01e79fa276b83be72dfb1
asda <y*********@gmail.com>
2025-03-16 18:50:49 +0400

    25

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -37,7 +37,7 @@
                     <path d="M3.00065168,20.1992055 C3.38825852,15.4265159 7.26191235,13 11.9833413,13 C16.7712164,13 20.7048837,15.2931929 20.9979143,20.2 C21.0095879,20.3954741 20.9979143,21 20.2466999,21 C16.541124,21 11.0347247,21 3.72750223,21 C3.47671215,21 2.97953825,20.45918 3.00065168,20.1992055 Z" fill="#000000" fill-rule="nonzero"/>
                 </g>
             </svg><!--end::Svg Icon--></span>
-            <span class="menu-text">X</span>
+            <span class="menu-text">Employee Management</span>
             <i class="menu-arrow"></i>
         </a>
         <div class="menu-submenu">
@@ -69,7 +69,7 @@
     </li>
 
 
-    <li class="menu-item menu-item-submenu" aria-haspopup="true" data-menu-toggle="hover">
+    {{-- <li class="menu-item menu-item-submenu" aria-haspopup="true" data-menu-toggle="hover">
         <a href="javascript:;" class="menu-link menu-toggle">
             <span class="svg-icon menu-icon svg-icon-primary svg-icon-2x"><!--begin::Svg Icon | path:D:\xampp\htdocs\keenthemes\legacy\keen\theme\demo1\dist/../src/media/svg/icons\General\User.svg--><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
                 <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
@@ -96,14 +96,14 @@
                         </i>
                         <span class="menu-text">Jess</span>
                     </a>
-                </li>               
+                </li>
             </ul>
         </div>
-    </li>
+    </li> --}}
 
 
 
-    <li class="menu-item menu-item-submenu" aria-haspopup="true" data-menu-toggle="hover">
+    {{-- <li class="menu-item menu-item-submenu" aria-haspopup="true" data-menu-toggle="hover">
         <a href="javascript:;" class="menu-link menu-toggle">
             <span class="svg-icon menu-icon svg-icon-primary svg-icon-2x">
                 <!--begin::Svg Icon | path:D:\xampp\htdocs\keenthemes\legacy\keen\theme\demo1\dist/../src/media/svg/icons\General\User.svg--><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
@@ -142,7 +142,7 @@
                 </li>
             </ul>
         </div>
-    </li>
+    </li> --}}
 
 </ul>
 <!--end::Menu Nav-->
